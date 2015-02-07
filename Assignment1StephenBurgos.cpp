83be5ce83f113c97adb37bb5ce12ea372144c3ca
Stephen Burgos <stephen.burgos782004@gmail.com>
2015-02-07 14:42:18 -0500

    Working
    Checked that the code meets the design and implementation part of the
    rubric.  Deleted a function that was not being used and a #define that
    was no longer used.  Edited comments to better reflect the comment
    coding standard defined in the header.

--- Assignment1StephenBurgos.cpp
+++ Assignment1StephenBurgos.cpp
@@ -1,33 +1,33 @@
-/* HEADER
+/* 
  * Author: Stephen Burgos
  * Class: CIS 363, Concurrent Programming
  * Professor: Dr. Joe Hoffert
  * Assignment 1: Chapter 1, Exercise 3, page 28
- * Last Edited: February 6, 2015
+ * Last Edited: February 7, 2015
  * Version: 1.0
- * Description: This program counts the number of 3s in a given array
-		and prints the final count.
+ * Description: This program generates an array of random numbers, 
+		prints the array, counts the number of 3s (or other value if specified)
+		in a given array and prints the final count.
  * Statement: I wrote all of this code myself and did not copy any
 		code from the internet or another student.
+ * Comment: It is hard to determine what a "Huge function" is, but I don't think any
+		of mine are.  
+ * Formatting Note: Comments that tell what a code block does are placed above the block,
+		comments that tell what a single line means are placed on that line
 */
 #include <iostream>
 #include <thread>
 #include <random>
-#include <array>
-
-#define ARRAY_SIZE(array) (sizeof((array)) / sizeof((array[0]))) // Gives the size of the array
-
 typedef const int CINT;
 
 void _addThrees(int i, int * arrayOne_arr, int * arrayTwo_arr)
 {
 	// Pre-condition: arrayOne contains integers that will be added together, 
-	// two at a time, and placed into arrayTwo
-	// Post-condition: The indicis in arrayTwo each contain two of the indices 
-	// from arrayOne added together. i.e., arrayTwo[0] contains arrayOne[0] + 
-	// arrayOne[1]
+	// two at a time, and placed into arrayTwo_arr
+	// Post-condition: The indices in arrayTwo_arr each contain two of the indices 
+	// from arrayOne added together. i.e., arrayTwo_arr[0] contains arrayOne_arr[0] + 
+	// arrayOne_arr[1]
 	arrayTwo_arr[i / 2] = arrayOne_arr[i - 1] + arrayOne_arr[i];
-	return; 
 }
 
 void _checkThree(int * arrayCheck_arr, CINT size_cint, CINT find_value_cint)
@@ -47,65 +47,41 @@ void _checkThree(int * arrayCheck_arr, CINT size_cint, CINT find_value_cint)
 			arrayCheck_arr[i] = 0;
 		}
 	}
-	return;
-}
-
-int _switchArray(int * arrayOne_arr, int * arrayTwo_arr)
-{
-	// Pre-condition: Two arrays are passed in.  The first array will become the second 
-	// and the second will be set to half the size of the first and become all 0s
-	// Post-condition: Two arrays are "returned" for use in the next iteration of counting
-	CINT size_of_one_cint = ARRAY_SIZE(arrayOne_arr);
-	CINT size_of_two_cint = ARRAY_SIZE(arrayTwo_arr);
-	for (int i = 0; i < size_of_two_cint; ++i)
-	{
-		arrayOne_arr[i] = arrayTwo_arr[i]; // Copy the elements back to one to be summed again
-	}
-	for (int i = size_of_two_cint; i < size_of_one_cint; ++i)
-	{
-		arrayOne_arr[i] = 0; // Set all other values in arrayOne_arr to 0
-	}
-	for (int i = 0; i < size_of_two_cint; ++i)
-	{
-		arrayTwo_arr[i] = 0; // Reset arrayTwo_arr
-	}
-	int used_space_int = size_of_two_cint;
-	return used_space_int; // Return the value to loop with in main, full value to loop through arrayOne_arr and half for arrayTwo_arr
 }
 
 int main()
 {
 	// Variable declaration
+	// The first 4 may be changed, based on preference, before executing the code
 	CINT generator_max_cint = 4;
 	CINT generator_min_cint = 2;
-	CINT half_of_array_cint = 10; // Twice this number will be the size of the array
-	CINT array_size_cint = 2 * half_of_array_cint;
+	CINT half_of_array_cint = 11; // Twice this number will be the size of the array
 	CINT find_this_cint = 3; // Count how many of this number there is in the array
-	CINT max_threads_cint = array_size_cint / 2; // TODO Maybe delete the /2
+	CINT array_size_cint = 2 * half_of_array_cint;
+	CINT max_threads_cint = array_size_cint / 2;
 	int space_used_int = half_of_array_cint; // Tracks the space of each array being used
 	int arrayOfThrees_arr[array_size_cint];
-	// Create an array of random numbers
+	// Generate an array of array_size_cint random numbers in the range generator_min_cint to generator_max_cint
 	std::random_device randomDevice;
 	std::mt19937_64 numberGenerated_int(randomDevice());
 	std::uniform_int_distribution<> distr(generator_min_cint, generator_max_cint);
 	for (int index = 0; index < array_size_cint; ++index)
 	{
-		// Generate an array of array_size_cint random numbers in the range generator_min_cint to generator_max_cint
 		arrayOfThrees_arr[index] = distr(numberGenerated_int);
+	}	
+	// Print the array for to check visually
+	std::cout << "Initial array: " << std::endl;
+	for (int i = 0; i < array_size_cint; ++i)
+	{
+		std::cout << arrayOfThrees_arr[i]; 
 	}
+	std::cout << std::endl;
 	// Convert the array for counting
 	_checkThree(arrayOfThrees_arr, array_size_cint, find_this_cint);
 	std::thread addition_threads[max_threads_cint];
-	//DEBUGGING
-	std::cout << "Array One: " << std::endl;
-	for (int i = 0; i < array_size_cint; ++i)
-	{
-		std::cout << arrayOfThrees_arr[i];
-	}
-	//DEBUGGING END
-	// Add up the number of threes
+	// Add up the number of threes (or other value of find_this_cint)
 	int arrayTwo_arr[half_of_array_cint] = { 0 };
-	while (space_used_int > 0) // If less than 2, there is only one item left in the array, and it is the number of 3s
+	while (space_used_int > 0) // If space_used_int is 0, then the final answer has been found
 	{
 		for (int i = 0; i < space_used_int; ++i)
 		{
@@ -115,40 +91,29 @@ int main()
 		{
 			addition_threads[i].join();
 		}
+		// Don't lose the last number of an odd length array
 		if (space_used_int != 1 && space_used_int % 2 != 0)
 		{
-			arrayTwo_arr[space_used_int - 2] += arrayTwo_arr[space_used_int - 1]; // Don't lose the last number of an odd length array
+			arrayTwo_arr[space_used_int - 2] += arrayTwo_arr[space_used_int - 1]; 
 			arrayTwo_arr[space_used_int - 1] = 0;
 		}
-		// Add condition here to check for odd number in arrayOfThrees, if odd, add arrayTwo[last] to arrayOfThrees[last-1]
-
+		// Copy the elements back to arrayOfThrees_arr to be summed again
 		for (int i = 0; i < space_used_int; ++i)
 		{
-			arrayOfThrees_arr[i] = arrayTwo_arr[i]; // Copy the elements back to one to be summed again
+			arrayOfThrees_arr[i] = arrayTwo_arr[i]; 
 		}
+		// Set all other values in arrayOne_arr to 0
 		for (int i = space_used_int; i < space_used_int * 2; ++i)
 		{
-			arrayOfThrees_arr[i] = 0; // Set all other values in arrayOne_arr to 0
+			arrayOfThrees_arr[i] = 0; 
 		}
+		// Reset arrayTwo_arr
 		for (int i = 0; i < space_used_int; ++i)
 		{
-			arrayTwo_arr[i] = 0; // Reset arrayTwo_arr
+			arrayTwo_arr[i] = 0; 
 		}
 		space_used_int = space_used_int / 2;
 	}
-	//DEBUGGING:
-	//std::cout << "Array One: " << std::endl;
-	//for (int i = 0; i < array_size_cint; ++i)
-	//{
-	//	std::cout << arrayOfThrees_arr[i];
-	//}
-	std::cout << std::endl;
-	std::cout << "Array Two: " << std::endl;
-	for (int i = 0; i < half_of_array_cint; ++i)
-	{
-		std::cout << arrayTwo_arr[i];
-	}
-	std::cout << std::endl;
-	std::cout << "Final value: " << arrayOfThrees_arr[0] << std::endl;
+	std::cout << "The final count for the number of " << find_this_cint << "s is " << arrayOfThrees_arr[0] << std::endl;
 	return 0;
 }
